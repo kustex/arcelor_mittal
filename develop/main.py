c58238123bc2131c5b4ccd93a5c196b22659e23e
Rosseel <rosseelbernard@hotmail.com>
2021-03-16 15:46:14 +0100

    created plot_min_max() to showcase scatterplot of B4min/max and B5min/max

--- develop/main.py
+++ develop/main.py
@@ -1,9 +1,19 @@
 from develop.dev import get_df_length_and_values
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
-path_b4 = '520677B4'
-path_b5 = '520677B5'
 
-length, width, df=get_df_length_and_values(path_b4)
-print(length)
-print(width)
-print(df)
+
+
+from develop.plotting import plot_min_max
+#
+df=pd.read_csv('CoilDataAugmented.csv')
+
+
+df['Cluster']=np.floor(np.random.rand(len(df),1)*9)
+df.Cluster=df.Cluster.astype(int)
+print(df.dtypes)
+plot_min_max(df)
+# plt.show()
