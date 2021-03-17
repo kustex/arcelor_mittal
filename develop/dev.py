eecb9b2e245f1b92703611045de1111727f02adc
Lucas Kustermans <LucasKustermans@Lucass-MacBook-Pro.local>
2021-03-17 11:59:02 +0100

    added functionality to get a dictionary with all max absolute differences between b4 and b5 for all the coils

--- develop/dev.py
+++ develop/dev.py
@@ -1,25 +1,21 @@
 import zipfile
 import re
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from scipy.interpolate import interp1d
 
-path_b4 = '520677B4'
-path_b5 = '520677B5'
 
 def get_df_length_and_values(path):
 	'''
 	- Reading, cleaning of CSV-file
-	- Linear interpolation of the data
 	- Return = 2 pandas Series 1. Length, 2. Values
-
 	'''
-	df = pd.read_csv(f'../data/SignalExport/{path}.csv', header=None, index_col=0, squeeze=True)
+	df = pd.read_csv(f'../test_data/{path}.csv', header=None, index_col=0, squeeze=True)
 	df = str(pd.Series(data=df.index).tolist())
 	df = re.split(';', df)[3:]
-
 	length_p = []
 	for i in df:
 		if i != 'Values':
@@ -41,61 +37,37 @@ def get_df_length_and_values(path):
 	return length_p, values, df
 
 def interpolate_values(x, y):
+	'''
+	Interpolation of the data so that we take the differences of b4 and b5.
+	'''
 	f = interp1d(x, y, fill_value="extrapolate")
-	xnew = np.arange(0, max(round(x, 2)), 0.01)
+	xnew = np.arange(0, 200, 0.1)
 	ynew = f(xnew)
 	return xnew, ynew
 
-df_b4 = get_df_length_and_values(path_b4)[2]
-df_b5 = get_df_length_and_values(path_b5)[2]
-
-x_b4 = get_df_length_and_values(path_b4)[0]
-x_b5 = get_df_length_and_values(path_b5)[0]
-y_b4 = get_df_length_and_values(path_b4)[1]
-y_b5 = get_df_length_and_values(path_b5)[1]
-
-intp_x_b4 = interpolate_values(x_b4, y_b4)[0]
-intp_x_b5 = interpolate_values(x_b5, y_b5)[0]
-intp_y_b4 = interpolate_values(x_b4, y_b4)[1]
-intp_y_b5 = interpolate_values(x_b5, y_b5)[1]
-
-difference_values_b4_b5 = [i - j for i, j in zip(intp_y_b4[200:], intp_y_b5[200:])]
-print(np.mean(difference_values_b4_b5))
-
-def get_mean_until_140(df_b4, df_b5):
-	lst_b4 = []
-	lst_b5 = []
-	for i, j in zip(df_b4['length_p'], df_b4['values']):
-		if i < 140:
-			lst_b4.append(j)
-		else:
-			break
-	for i, j in zip(df_b5['length_p'], df_b5['values']):
-		if i < 140:
-			lst_b5.append(j)
-		else:
-			break
-	b4_mean = np.mean(lst_b4)
-	b5_mean = np.mean(lst_b5)
-	return b4_mean, b5_mean
-
-def get_values_between_140_and_170(df_b4, df_b5):
-	lst_b4 = []
-	lst_b5 = []
-	for i, j in zip(df_b4['length_p'], df_b4['values']):
-		if i >= 140 and i <= 170:
-			lst_b4.append(j)
-	for i, j in zip(df_b5['length_p'], df_b5['values']):
-		if i >= 140 and i <= 170:
-			lst_b5.append(j)
-	return len(lst_b4), len(lst_b5)
-
-fig, ax = plt.subplots(figsize=(20,10))
-l1, = ax.plot(df_b4['length_p'][10:], df_b4['values'][10:])
-l2, = ax.plot(df_b5['length_p'][10:], df_b5['values'][10:])
-l3, = ax.plot(intp_x_b5[1000:], intp_y_b5[1000:])
-ax.legend([l1, l2, l3], ['B4','B5', 'interp B5'])
-plt.title('B4 and B5 measurements')
-plt.xlabel('Length metal plate')
-plt.ylabel('Width metal plate')
-plt.show()
+def get_dict_abs_differences():
+	'''
+	This function returns the max absolute difference between b4 and b5 in a dictionary, at the interval of 140m until 170m,
+	for all the coils in the data folder.
+	'''
+	column_list = os.listdir('../test_data')
+	newlist = [name for name in column_list if name.endswith("B4.csv")]
+	newdict = {}
+	for name_b4 in newlist:
+		for name_b5 in column_list:
+			if name_b5[:-5] == name_b4[:-5] and name_b5 != name_b4:
+				b4_x = get_df_length_and_values(name_b4[:-4])[0]
+				b4_y = get_df_length_and_values(name_b4[:-4])[1]
+				b5_x = get_df_length_and_values(name_b5[:-4])[0]
+				b5_y = get_df_length_and_values(name_b5[:-4])[1]
+				if len(b4_x) > 0 and len(b5_x) > 0:
+					b4_x_values = [i for i in b4_x if i > 0]
+					b4_y_values = [i for i in b4_y if i > 0]
+					b5_x_values = [i for i in b5_x if i > 0]
+					b5_y_values = [i for i in b5_y if i > 0]
+					b4_l = interpolate_values(b4_x_values, b4_y_values)[1]
+					b5_l = interpolate_values(b5_x_values, b5_y_values)[1]
+					difference_b4_b5 = [abs(i-j) for i, j in zip(b4_l[140:170], b5_l[140:170])]
+					max_abs_diff = max(difference_b4_b5)
+					newdict[f'{name_b4[:-6]}'] = max_abs_diff
+	return newdict
