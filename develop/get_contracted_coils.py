d8b27f097b10e4780b88f7fb74fca3b6b8c155ad
Lucas Kustermans <LucasKustermans@Lucass-MacBook-Pro.local>
2021-03-18 11:50:35 +0100

    cleanup and renaming of files

--- develop/get_contracted_coils.py
+++ develop/get_contracted_coils.py
@@ -1,5 +1,3 @@
-import zipfile
-import re
 import csv
 import os
 import pandas as pd
@@ -7,38 +5,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib. pyplot as plt
 
-from scipy.interpolate import interp1d
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-
-
-def get_df_length_and_values(path):
-	'''
-	- Reading, cleaning of CSV-file
-	- Return = 2 pandas Series 1. Length, 2. Values
-	'''
-	df = pd.read_csv(f'../data/SignalExport/{path}.csv', header=None, index_col=0, squeeze=True)
-	df = str(pd.Series(data=df.index).tolist())
-	df = re.split(';', df)[3:]
-	length_p = []
-	for i in df:
-		if i != 'Values':
-			length_p.append(i)
-		else:
-			break
-	length_p = length_p[:-1]
-	length_p = [float(i) for i in length_p]
-	values = df[len(length_p)+3:][:-1]
-	values = [float(i) for i in values]
-	values = [i for i in values if i != 0]
-	length_p = [i for i in length_p if i != 0]
-
-	df = list(zip(length_p, values))
-	df = pd.DataFrame(data=df, columns=['length_p','values'])
-
-	length_p = df['length_p']
-	values = df['values']
-	return length_p, values, df
+from csv_reader import csv_to_df
 
 def interpolate_values(x, y):
 	'''
@@ -60,10 +27,10 @@ def get_dict_abs_differences():
 	for name_b4 in newlist:
 		for name_b5 in column_list:
 			if name_b5[:-5] == name_b4[:-5] and name_b5 != name_b4:
-				b4_x = get_df_length_and_values(name_b4[:-4])[0]
-				b4_y = get_df_length_and_values(name_b4[:-4])[1]
-				b5_x = get_df_length_and_values(name_b5[:-4])[0]
-				b5_y = get_df_length_and_values(name_b5[:-4])[1]
+				b4_x = csv_to_df(name_b4[:-4])[0]
+				b4_y = csv_to_df(name_b4[:-4])[1]
+				b5_x = csv_to_df(name_b5[:-4])[0]
+				b5_y = csv_to_df(name_b5[:-4])[1]
 				if len(b4_x) > 0 and len(b5_x) > 0:
 					b4_x_values = [i for i in b4_x if i > 0]
 					b4_y_values = [i for i in b4_y if i > 0]
