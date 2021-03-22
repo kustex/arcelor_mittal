d7f13fd6c0a8b39892c0cf4ea8d0a7a8a8516063
Lucas Kustermans <44235638+kustex@users.noreply.github.com>
2021-03-22 16:17:00 +0100

    Delete exploration.py

--- develop/exploration.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from dev import get_five_perc_highest_abs_diff, get_dict_abs_differences
-
-contracted_coils = get_five_perc_highest_abs_diff(get_dict_abs_differences())
-data = pd.read_csv('../data/CoilData.csv')
-data = data[data['coil'].isin(contracted_coils)]
